04db29bb2b23dd997160524bae6f2a2d4646d4d4
Adam-Koza <adam.koza@mail.utoronto.ca>
2019-02-15 21:35:18 -0500

    Ethers initalize sample.

--- Demo/test/contract_spec.js
+++ Demo/test/contract_spec.js
@@ -1,17 +1,18 @@
 // /*global contract, config, it, assert*/
 
 const SimpleDemo = require('Embark/contracts/SimpleDemo');
+const ethers = require('ethers');
 
 let accounts;
 
 // For documentation please see https://embark.status.im/docs/contracts_testing.html
 config({
   deployment: {
-   accounts: [
-    {mnemonic: "spirit helmet trim rabbit little obtain air minimum ancient child island stuff"}
-     // you can configure custom accounts with a custom balance
-     // see https://embark.status.im/docs/contracts_testing.html#Configuring-accounts
-   ]
+    accounts: [
+      { mnemonic: "spirit helmet trim rabbit little obtain air minimum ancient child island stuff" }
+      // you can configure custom accounts with a custom balance
+      // see https://embark.status.im/docs/contracts_testing.html#Configuring-accounts
+    ]
   },
   contracts: {
     "SimpleDemo": {
@@ -28,21 +29,35 @@ contract("SimpleDemo", function () {
   it("contract has been deployed.", async function () {
     let addressOfContract = SimpleDemo.options.address;
     console.log(addressOfContract);
-    assert.ok(addressOfContract.length >0);
+    assert.ok(addressOfContract.length > 0);
   });
 
 
   it("Big Numbers.", async function () {
+    const BNweb = web3.utils.BN;
+    const BNethers = ethers.utils.bigNumberify;
+
+    new BNweb(1234).toString();
+    new BNethers(1234)
+// "1234"
+
+    new BNweb('1234').add(new BNweb('1')).toString();
+    new BNethers('1234').add(new BNethers('1')).toString();
+// "1235"
+
+    new BNweb('0xea').toString();
+    new BNethers('0xea').toString();
+// "234"
     let addressOfContract = SimpleDemo.options.address;
     console.log(addressOfContract);
-    assert.ok(addressOfContract.length >0);
+    assert.ok(addressOfContract.length > 0);
   });
 
 
   it("Sign Message.", async function () {
     let addressOfContract = SimpleDemo.options.address;
     console.log(addressOfContract);
-    assert.ok(addressOfContract.length >0);
+    assert.ok(addressOfContract.length > 0);
   });
 
 
