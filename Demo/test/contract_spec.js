f8e7d5a6f32b0ae5811419cd9ca8c96b6d7b8516
Adam-Koza <adam.koza@mail.utoronto.ca>
2019-02-15 21:55:30 -0500

    Sign message test.

--- Demo/test/contract_spec.js
+++ Demo/test/contract_spec.js
@@ -39,14 +39,14 @@ contract("SimpleDemo", function () {
 
     let num1 = new BNweb(1234).toString();
     let num2 = new BNethers(1234).toString();
-// "1234"
+    // "1234"
     assert.ok(num1 == num2);
 
 
     let num3 = new BNweb('1234').add(new BNweb('1')).toString();
-    let num4 = new  BNethers('1234').add(new BNethers('1')).toString();
+    let num4 = new BNethers('1234').add(new BNethers('1')).toString();
 
-// "1235"
+    // "1235"
     assert.ok(num3 == num4);
 
     let num5 = new BNweb('0xea').toString();
@@ -55,15 +55,35 @@ contract("SimpleDemo", function () {
     console.log(num5);
     console.log(num6);
 
-// "234"
+    // "234"
     assert.ok(num5 == num6);
   });
 
 
   it("Sign Message.", async function () {
-    let addressOfContract = SimpleDemo.options.address;
-    console.log(addressOfContract);
-    assert.ok(addressOfContract.length > 0);
+    // Create a wallet to sign the message with
+    let privateKey = '0x0123456789012345678901234567890123456789012345678901234567890123';
+    let ethersWallet = new ethers.Wallet(privateKey);
+    let message = "Hello World";
+
+    // Sign the string message
+    let flatSig = await ethersWallet.signMessage(message);
+
+    // For Solidity, we need the expanded-format of a signature
+    let ethersSig = ethers.utils.splitSignature(flatSig);
+
+
+    let webSig = await web3.eth.accounts.sign(message, privateKey);
+
+    console.log("");
+    console.log("Ethers sig:");
+    console.log("");
+    console.log(ethersSig);
+    console.log("");
+    console.log("Web3 sig:");
+    console.log("");
+    console.log(webSig);
+
   });
 
 
